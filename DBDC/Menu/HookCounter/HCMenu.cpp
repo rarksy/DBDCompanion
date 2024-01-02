98c65552994d3d4b662886b40db7f84fdd4a5539
rarksy <rrarksy@gmail.com>
2024-01-02 21:12:39 +1000

    big fixes

--- DBDC/Menu/HookCounter/HCMenu.cpp
+++ DBDC/Menu/HookCounter/HCMenu.cpp
@@ -1,11 +1,11 @@
-﻿
-#include "HCMenu.h"
-
+﻿#include "HCMenu.h"
 #include "ImGui/imgui.h"
+#include <Windows.h>
 
 void HCMenu::RenderUI()
 {
     ImGui::Text("hook counter menu");
+    
 }
 
 void HCMenu::CreateStyle()
