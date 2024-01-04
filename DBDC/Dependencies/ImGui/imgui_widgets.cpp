9e20bfc23a511cd115007a125fb74e1fd3394536
rarksy <rrarksy@gmail.com>
2024-01-04 22:57:18 +1000

    fixed buttons not using framebordersize

--- DBDC/Dependencies/ImGui/imgui_widgets.cpp
+++ DBDC/Dependencies/ImGui/imgui_widgets.cpp
@@ -707,7 +707,7 @@ bool ImGui::ButtonEx(const char* label, const ImVec2& size_arg, ImGuiButtonFlags
     const ImU32 col = GetColorU32((held && hovered) ? ImGuiCol_ButtonActive : hovered ? ImGuiCol_ButtonHovered : ImGuiCol_Button);
     RenderNavHighlight(bb, id);
     //RenderFrame(bb.Min, bb.Max, col, true, style.FrameRounding);
-    window->DrawList->AddRect(bb.Min, bb.Max, col, style.FrameRounding);
+    window->DrawList->AddRect(bb.Min, bb.Max, col, style.FrameRounding, NULL, style.FrameBorderSize);
 
     if (g.LogEnabled)
         LogSetNextTextDecoration("[", "]");
