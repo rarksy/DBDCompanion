e834cf8a392ba128c4f163b1ee67f02ad1fc66e5
Rarksy <70506725+rarksy@users.noreply.github.com>
2024-03-19 16:27:44 +1000

    initial setup + caching

--- DBDC/Features/Perk Packager/PerkPackager.cpp
+++ DBDC/Features/Perk Packager/PerkPackager.cpp
@@ -1,8 +1,38 @@
 ﻿#include "PerkPackager.h"
 
 #include "PPMenu.h"
+#include "../../Backend/Backend.hpp"
 
-void PerkPackager::Setup()
+void perk_packager::setup()
 {
+    const std::string character_data_file_path = backend::exe_directory.string() + backend::settings_directory + "character_data.json";
+    const std::string perk_data_file_path = backend::exe_directory.string() + backend::settings_directory + "perk_data.json";
 
+    const int time_since_cache = ml::get_seconds_since_file_modified(character_data_file_path);
+    if (time_since_cache > 86400 /* 1d */ || time_since_cache == -1)
+    {
+        _internal::all_characters_data = ml::json_get_from_url("https://dbd.tricky.lol/api/characters");
+        _internal::all_perks_data = ml::json_get_from_url("https://dbd.tricky.lol/api/perks");
+        ml::json_write_data(character_data_file_path, _internal::all_characters_data);
+        ml::json_write_data(perk_data_file_path, _internal::all_perks_data);
+    }
+    else
+    {
+        _internal::all_characters_data = ml::json_get_data_from_file(character_data_file_path);
+        _internal::all_perks_data = ml::json_get_data_from_file(perk_data_file_path);
+    }
+
+    for (const auto& character : _internal::all_characters_data)
+    {
+        Character c;
+
+        c.name = character["name"];
+        c.id = character["id"];
+        c.role = character["role"];
+
+        for (int i = 0; i < character["perks"].size(); i++)
+            c.all_perks.push_back(character["perks"][i]);
+
+        all_characters.push_back(c);
+    }
 }
--- DBDC/Features/Perk Packager/PerkPackager.h
+++ DBDC/Features/Perk Packager/PerkPackager.h
@@ -2,7 +2,22 @@
 
 #include "miscLIB/miscLIB.hpp"
 
-namespace PerkPackager
+namespace perk_packager
 {
-    void Setup();
+    void setup();
+
+    struct Character
+    {
+        std::string name;
+        std::string id;
+        std::string role;
+        std::vector<std::string> all_perks;
+    };
+    inline std::vector<Character> all_characters;
+
+    namespace _internal
+    {
+        inline nlohmann::json all_characters_data;
+        inline nlohmann::json all_perks_data;
+    }
 }
