a548836885e7af02210351e9fa767d60683447fb
Rarksy <70506725+rarksy@users.noreply.github.com>
2024-03-29 15:11:11 +1000

    refactor

--- DBDC/Misc/Misc.cpp
+++ DBDC/Misc/Misc.cpp
@@ -12,7 +12,7 @@
 #include "miscLIB/miscLIB.hpp"
 #include "zlib/zlib.h"
 
-void misc::RestartGame(bool dx12)
+void misc::restart_game()
 {
     if (ml::is_exe_running(L"DeadByDaylight-Win64-Shipping.exe"))
     {
@@ -26,11 +26,8 @@ void misc::RestartGame(bool dx12)
             CloseHandle(handle);
         }
     }
-
-    std::string renderApi = dx12 ? " -DX12" : "";
-    std::string runString = "steam://rungameid/381210" + renderApi;
-
-    ShellExecuteA(NULL, "open", runString.c_str(), NULL, NULL, SW_SHOWDEFAULT);
+    
+    ShellExecuteA(NULL, "open", "steam://rungameid/381210", NULL, NULL, SW_SHOWDEFAULT);
 }
 
 cv::Mat misc::get_screenshot(const cv::Rect& region, bool grayscale)
@@ -64,7 +61,7 @@ cv::Mat misc::get_screenshot(const cv::Rect& region, bool grayscale)
     return screenshot;
 }
 
-std::vector<std::string> misc::GetAllLibraryDirectories()
+std::vector<std::string> misc::get_all_steam_library_directories()
 {
     std::vector<std::string> paths;
     std::ifstream configFile("C:\\Program Files (x86)\\Steam\\steamapps\\libraryfolders.vdf"); // HATE this
@@ -109,7 +106,7 @@ std::string misc::get_game_root_directory()
     // C:\Program Files (x86)\Steam\steamapps/libraryfolders.vdf
     // use it to check all steam libraries for dbd root directory
 
-    const std::vector<std::string> allPaths = GetAllLibraryDirectories();
+    const std::vector<std::string> allPaths = get_all_steam_library_directories();
 
     for (const auto& path : allPaths)
     {
