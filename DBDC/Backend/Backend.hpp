fc4cdfa4b8a816359c833292c76a512a0e8fbe1f
Rarksy <70506725+rarksy@users.noreply.github.com>
2024-04-27 17:01:59 +1000

    v0.10

--- DBDC/Backend/Backend.hpp
+++ DBDC/Backend/Backend.hpp
@@ -3,7 +3,7 @@
 #include <GLFW/glfw3.h>
 #include "ImGui/imgui_internal.h"
 
-#define DBDC_VERSION std::string("master_v0.9")
+#define DBDC_VERSION std::string("master_v0.10")
 
 namespace backend
 {
