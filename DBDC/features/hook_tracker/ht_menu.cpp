6d0146238a99219a0215e7709d9756691a650e7f
Rarksy <70506725+rarksy@users.noreply.github.com>
2024-05-17 01:20:57 +1000

    setup color saving/loading

--- DBDC/features/hook_tracker/ht_menu.cpp
+++ DBDC/features/hook_tracker/ht_menu.cpp
@@ -5,6 +5,7 @@
 #include "hook_tracker.hpp"
 #include "..\config_editor\config_editor.hpp"
 #include "GUI/GUI.h"
+#include "miscLIB/miscLIB.hpp"
 #include "nlohmann/json.hpp"
 #include "overlay/overlay.hpp"
 
@@ -13,6 +14,21 @@ void ht_menu::setup()
     config_editor::initialize_config();
 
     hook_tracker::ht_vars::in_game_ui_scale.load_value();
+
+    const auto color_data = ml::json_get_data_from_file(backend::exe_directory.string() + backend::settings_directory + backend::data_directory + "settings.json");
+
+    if (color_data.contains("hook_tracker"))
+    {
+        stage_1_color.r = color_data["hook_tracker"]["stage_1_color"]["r"];
+        stage_1_color.g = color_data["hook_tracker"]["stage_1_color"]["g"];
+        stage_1_color.b = color_data["hook_tracker"]["stage_1_color"]["b"];
+        stage_1_color.a = color_data["hook_tracker"]["stage_1_color"]["a"];
+
+        stage_2_color.r = color_data["hook_tracker"]["stage_2_color"]["r"];
+        stage_2_color.g = color_data["hook_tracker"]["stage_2_color"]["g"];
+        stage_2_color.b = color_data["hook_tracker"]["stage_2_color"]["b"];
+        stage_2_color.a = color_data["hook_tracker"]["stage_2_color"]["a"];
+    }
 
     hook_tracker::setup();
 }
@@ -47,6 +63,27 @@ void ht_menu::render_ui()
 
     if (gui::drop_down_box("In-Game UI Scale", ui_scale_index, UIScales, true, 40))
         hook_tracker::setup();
+    gui::tool_tip("Sets the position that tracked hooks will be shown at\n\nNote: This SHOULD be automatically set to what your in game value is set to.");
+
+    const auto save_color = [](std::string setting, color col)
+    {
+        nlohmann::json data;
+
+        data["hook_tracker"][setting]["r"] = stage_2_color.r;
+        data["hook_tracker"][setting]["g"] = stage_2_color.g;
+        data["hook_tracker"][setting]["b"] = stage_2_color.b;
+        data["hook_tracker"][setting]["a"] = stage_2_color.a;
+
+        ml::json_write_data(backend::exe_directory.string() + backend::settings_directory + backend::data_directory + "settings.json", data);
+    };
+
+    if (gui::color_picker("Stage 1 Color", &stage_1_color, true))
+        save_color("stage_1_color", stage_1_color);
+    gui::tool_tip("Sets the color that first stage hooks will be shown as.");
+    
+    if (gui::color_picker("Stage 2 Color", &stage_2_color, true))
+        save_color("stage_2_color", stage_2_color);
+    gui::tool_tip("Sets the color that second stage hooks will be shown as.");
 
     ImGui::SeparatorText("Keybinds");
 
--- DBDC/features/hook_tracker/ht_menu.h
+++ DBDC/features/hook_tracker/ht_menu.h
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 
+#include "color/color.hpp"
+
 
 namespace ht_menu
 {
@@ -10,4 +12,7 @@ namespace ht_menu
 
     inline int ui_scale_index = -1;
     inline std::vector<std::string> UIScales = {"70", "75", "80", "85", "90", "95", "100"};
+
+    inline color stage_1_color = color(255, 255, 255);
+    inline color stage_2_color = color(255, 255, 255);
 }
